814a2eadbbe4b1223ef69885415299ae486c310f
vatsalkudecha <1311vatsal@gmail.com>
2024-09-09 00:40:45 +0530

    Built header

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="">
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/04bef84d-51f6-401e-9b8e-4a521cbce3c5/null/IN-en-20240903-TRIFECTA-perspective_0d3aac9c-578f-4e3c-8aa8-bbf4a392269b_large.jpg" alt="logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
